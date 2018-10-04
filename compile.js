2e13d67a45046b42153f465c044a9b4554fc77d9
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2018-10-04 17:19:05 -0300

    Modulo para compilacao feito

--- compile.js
+++ compile.js
@@ -1,2 +1,13 @@
 const path = require('path');
 const fs = require('fs');
+const solc = require('solc');
+
+/* Criaçāo de caminho para contrato a partir de de compile.js 
+__dirname leva até diretório raíz da aplicaçāo */
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+
+/* Para ler o conteúdo dos arquivos com FileSync module */
+const source = fs.readFileSync(inboxPath,'utf8');
+
+/* Para compilar */ 
+module.exports = solc.compile(source, 1).contracts[':Inbox']; 
--- contracts/inbox.sol
+++ contracts/inbox.sol
@@ -1,4 +1,4 @@
-pragma solidity ˆ0.4.17;
+
 
 contract Inbox {
     string public message;
